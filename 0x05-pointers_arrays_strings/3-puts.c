270ade3b90b92c951f99e3cd82ed9fa1769d83c8
vintops <twofortytwo641@gmail.com>
2023-06-26 09:22:11 -0700

    task iii

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ * Return: On success 1
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+
+	}
+
+		_putchar('\n');
+}
+
