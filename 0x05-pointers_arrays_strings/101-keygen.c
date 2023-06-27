259f9ef384c8378cb5d3cc35fdf6aea45ad274b4
vintops <twofortytwo641@gmail.com>
2023-06-26 18:14:35 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -33,7 +33,7 @@ int main(void)
 		if ((sum - 2772) % 2 != 0)
 		diff_half1++;
 
-		for (index = 0; passowrd[index]; index++)
+		for (index = 0; password[index]; index++)
 
 		{
 			if (password[index] >= (33 + diff_half1))
