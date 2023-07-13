a171cb5641804198a0f368fa1282ef7ab31cb027
vintops <twofortytwo641@gmail.com>
2023-07-13 06:37:18 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* malloc_checked - Allocates memory
+* @b: The size to allocate
+*
+* Return: Nothing
+*/
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+
+if (ptr == NULL)
+{
+fprintf(stderr, "Memory allocation failed. Exiting with status 98.\n");
+exit(98);
+}
+return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,4 +1,4 @@
-ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 void *malloc_checked(unsigned int b);
